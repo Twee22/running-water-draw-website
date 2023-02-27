390bd9f1c6f3bb15c492d0e792c9dc195850ce6f
BadConnection <59586452+BadConnection@users.noreply.github.com>
2023-02-26 22:18:40 -0600

    Create Application.html and add basic html for App

--- app.py
+++ app.py
@@ -1,4 +1,4 @@
-from flask import Flask, render_template, url_for
+from flask import Flask, render_template, url_for, request
 
 app = Flask(__name__)
 
@@ -7,7 +7,10 @@ app = Flask(__name__)
 def index():
     return render_template('index.html')
 
+@app.route('/application')
+def application():
+    return render_template('application.html')
+
 if __name__ == "__main__":
     app.run(debug=True)
 
-    
--- app/routes.py
+++ app/routes.py
@@ -1,7 +1,11 @@
-from flask import render_template
+from flask import render_template, request
 from app import app
 
 @app.route('/')
 @app.route('/index')
 def index():
     return render_template('index.html')
+
+@app.route('/application')
+def application():
+    return render_template('application.html')
