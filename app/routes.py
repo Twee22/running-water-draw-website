fd8e8061fb7fde4d9f86ea53b50f49ed9985e208
DainTwee <41553153+DainTwee@users.noreply.github.com>
2023-02-26 19:40:03 -0600

    Updates by Sean, left on my computer, should not effect much

--- app/routes.py
+++ app/routes.py
@@ -2,6 +2,6 @@ from flask import render_template
 from app import app
 
 @app.route('/')
-@app.route('/index')
+@app.route('/temp_index')
 def index():
     return render_template('temp_index.html')
