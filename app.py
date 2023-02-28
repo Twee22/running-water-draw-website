39a5773de0b529dabdc25f2a76d12d3c8426f785
BadConnection <59586452+BadConnection@users.noreply.github.com>
2023-02-28 00:39:14 -0600

    Fix css link

--- app.py
+++ app.py
@@ -1,5 +1,5 @@
-from flask import Flask, render_template, url_for, request, redirect
-from app.forms import ApplicationForm
+from flask import Flask, render_template, url_for
+
 app = Flask(__name__)
 
 if __name__ == "__main__":
