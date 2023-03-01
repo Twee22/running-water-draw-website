e295898a8ed66bf37fb2e67cc52b6d98375059fa
BadConnection <59586452+BadConnection@users.noreply.github.com>
2023-03-01 14:22:52 -0600

    Add DB Interface to Application

--- app/models.py
+++ app/models.py
@@ -8,3 +8,20 @@ class User(db.Model):
 
     def __repr__(self):
         return '<User {}>'.format(self.username)
+    
+class Vendor(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    user_id = db.Column(db.Integer, db.ForeignKey('user.id'))
+    name = db.Column(db.String(50), nullable=False)
+    business = db.Column(db.String(200), nullable=False)
+    address = db.Column(db.String(200), nullable=False)
+    citystatezip = db.Column(db.String(50), nullable=False)
+    email = db.Column(db.String(120), nullable=False, unique=True)
+    phoneNum = db.Column(db.String(20), nullable=False)
+    desc = db.Column(db.Text)
+    boothNum = db.Column(db.Integer, nullable=False)
+    tableNum = db.Column(db.Integer)
+
+    def __repr__(self):
+        return '<Vendor {}>'.format(self.business)
+    
--- app/routes.py
+++ app/routes.py
@@ -1,6 +1,7 @@
 from flask import render_template, request
 from app import app
 from app.forms import ApplicationForm
+from app.models import Vendor
 
 @app.route('/')
 @app.route('/index')
@@ -10,7 +11,18 @@ def index():
 @app.route('/application', methods=['GET', 'POST'])
 def application():
     form = ApplicationForm()
+    name = form.name
+    business = form.business
+    address = form.address
+    citystatezip = form.citystatezip
+    email = form.email
+    phoneNum = form.phoneNum
+    desc = form.desc
+    boothNum = form.boothNum
+    tableNum = form.tableNum
+    
     if form.is_submitted():
+        v = Vendor(name=name, business=business, email=email, address=address, citystatezip=citystatezip, phoneNum=phoneNum, desc=desc, boothNum=boothNum, tableNum=tableNum, )
         result = request.form
         return render_template('index.html', result=result)
     return render_template('application.html', form=form)
