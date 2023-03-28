45ea9e8059336d2e349d8db015240d2faf2b547f
BadConnection <59586452+BadConnection@users.noreply.github.com>
2023-03-27 19:18:08 -0500

    Add notes for rewriting map view for Sean

---
+++ app/vendor_dict.py
@@ -0,0 +1,5 @@
+vendor_dict = {{"filler_1": {"booth_num": 0, "booth_name": "NOTINIT", "status": "FILLER"}},
+                   {"booth_128": {"booth_num": 128, "booth_name": "NOTINIT", "status": "NOTINIT"}},
+                   {"filler_2": {"booth_num": 0, "booth_name": "NOTINIT", "status": "FILLER"}},
+                   {"booth_115": {"booth_num": 115, "booth_name": "NOTINIT", "status": "NOTINIT"}}
+                   }
