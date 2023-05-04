0ac5730ac4ff31a6a0ae7c2bbea6c18c554f2598
seanfitz922 <seanfitz922@gmail.com>
2023-05-04 18:34:47 -0500

    move email button, fix coloring on confirmation

--- app/models.py
+++ app/models.py
@@ -10,7 +10,7 @@ user_vendor = db.Table('user_vendor',
 class User(db.Model):
     id = db.Column(db.Integer, primary_key=True)
     username = db.Column(db.String(64), index=True, unique=True)
-    email = db.Column(db.String(120), index=True, unique=True)
+    email = db.Column(db.String(120), index=True)
     password_hash = db.Column(db.String(128))
     following = db.relationship('Vendor', secondary=user_vendor, backref='followers')
 
@@ -29,7 +29,7 @@ class Vendor(db.Model):
     business = db.Column(db.String(200), nullable=False)
     address = db.Column(db.String(200), nullable=False)
     citystatezip = db.Column(db.String(50), nullable=False)
-    email = db.Column(db.String(120), nullable=False, unique=True)
+    email = db.Column(db.String(120), nullable=False)
     phoneNum = db.Column(db.String(20), nullable=False)
     desc = db.Column(db.Text)
     boothNum = db.Column(db.Integer, nullable=False)
