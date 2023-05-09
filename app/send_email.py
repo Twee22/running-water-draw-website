a95cbdc84aa44c5a466bac53b674d11f4572def5
seanfitz922 <seanfitz922@gmail.com>
2023-05-08 23:49:01 -0500

    update wording of send_email

--- app/send_email.py
+++ app/send_email.py
@@ -9,8 +9,8 @@ app.config['MAIL_SERVER'] = 'smtp.gmail.com'
 app.config['MAIL_PORT'] = 465
 app.config['MAIL_USE_TLS'] = False
 app.config['MAIL_USE_SSL'] = True
-app.config['MAIL_USERNAME'] =  Config.email# enter your email address here
-app.config['MAIL_PASSWORD'] =  Config.email_password# enter your email password here
+app.config['MAIL_USERNAME'] =  Config.email
+app.config['MAIL_PASSWORD'] =  Config.email_password
 
 # create mail instance
 mail = Mail(app)
@@ -18,29 +18,31 @@ mail = Mail(app)
 # route to send email
 def send_email(user):
     invoice_payment = user.payment_amount
-    paypal_link = url_for('payment', id = user.id, _external=True)
+    paypal_link = url_for('payment', id=user.id, _external=True)
 
     # create message object
     msg = Message('Vendor Application Approved', sender='testemailschool@gmail.com', recipients=[user.email])
-    msg.body = f'Your vendor application has been approved, your invoice amount is ${str(invoice_payment)}. Here is a link to your invoice: {paypal_link}'
 
+    msg.body = f"Dear {user.name},\n\nWe're excited to inform you that your vendor application has been approved! Your invoice amount is ${str(invoice_payment)} and we've generated a PayPal invoice for you to make the payment process quick and easy. To access your invoice, simply click on this link: {paypal_link}. If you have any questions or concerns about the payment process, don't hesitate to reach out to us for assistance.\n\nThank you for joining us and we look forward to working with you!\n\nBest regards"
     # send email
     mail.send(msg)
 
 # route to send email 
 def send_payment_confirmation_email(user):
     # create message object
-    msg = Message('Payment Recieved', sender ='testemailschool@gmail.com', recipients=[user.email])
-    msg.body = 'We have recieved your invoice payment and your booth selection has been confirmed.'
+    msg = Message('Payment Received', sender='testemailschool@gmail.com', recipients=[user.email])
 
+    msg.body = f"Dear {user.name},\n\nWe're delighted to inform you that we've received your invoice payment and your booth selection has been confirmed. Thank you for completing the payment process on time.\n\nIf you have any questions or concerns, feel free to contact us for assistance.\n\nBest regards"
     # send confirmation of payment email
     mail.send(msg)
 
 
 def send_decline_email(user):
-    msg = Message('Vendor Application Declined', sender ='testemailschool@gmail.com', recipients=[user.email])
-    msg.body = 'Unfortunately, your vendor booth application has been declined. If you have questions please email us.'
+    # create message object
+    msg = Message('Vendor Application Declined', sender='testemailschool@gmail.com', recipients=[user.email])
 
+    msg.body = f"Dear {user.name},\n\nWe're sorry to inform you that your vendor booth application has been declined. If you have any questions or would like more information, please don't hesitate to reach out to us.\n\nThank you for your interest in our event.\n\nBest regards,\n[Your Name]"
+    # send declination of payment email
     mail.send(msg)
 
 
