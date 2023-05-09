df0dfae795142406b742fbb5e7966f8d51aeaa00
Twee22 <ilanjoffe2@gmail.com>
2023-05-08 20:04:15 -0500

    Move details to config file

--- config.py
+++ config.py
@@ -2,8 +2,12 @@ import os
 basedir = os.path.abspath(os.path.dirname(__file__))
 
 class Config(object):
-    SECRET_KEY = os.environ.get('SECRET_KEY') or 'you-will-never-guess'
+    SECRET_KEY = os.environ.get('SECRET_KEY') or 'pS^":$j$runAgqGLfCe}/T_`(b-HK'
     # ...
     SQLALCHEMY_DATABASE_URI = os.environ.get('DATABASE_URL') or \
         'sqlite:///' + os.path.join(basedir, 'app.db')
     SQLALCHEMY_TRACK_MODIFICATIONS = False
+    admin_username = "admin"
+    admin_password = "downward-dentist-crisis"
+    email = 'testemailschool50@gmail.com'
+    email_password = 'zktfwkjnfxnnttfk'
